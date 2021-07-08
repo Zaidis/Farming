6f3f233dfcaf67a26b5889fb40f0e755f5cd872e
Zaidis <chewybinx@gmail.com>
2021-07-08 01:30:52 -0500

    Added tree assets, skjybox assets, and other textures. added to map

--- Farming Game/Assets/Scripts/CameraLook.cs
+++ Farming Game/Assets/Scripts/CameraLook.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class CameraLook : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+
+    public float mouseSensitivy = 100f;
+
+    public Transform player;
+    float xRotation = 0f;
+
+    private void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update() {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
