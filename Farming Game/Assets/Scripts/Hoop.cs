64a9c3c7b0a5fa82dee7924ff4abd3d11f56cff6
Zaidis <chewybinx@gmail.com>
2021-07-09 01:53:08 -0500

    basketball hoop

---
+++ Farming Game/Assets/Scripts/Hoop.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Hoop : MonoBehaviour
+{
+    public int hoopCounter;
+    public TextMeshPro cc;
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Item")) {
+            hoopCounter++;
+        }
+        cc.text = hoopCounter.ToString();
+    }
+
+}
