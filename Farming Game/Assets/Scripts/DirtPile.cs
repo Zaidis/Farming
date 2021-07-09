49dbe5c24a8b8c77f9dadceb2730265114370c7a
Zaidis <chewybinx@gmail.com>
2021-07-09 01:31:02 -0500

    farming works

--- Farming Game/Assets/Scripts/DirtPile.cs
+++ Farming Game/Assets/Scripts/DirtPile.cs
@@ -6,13 +6,25 @@ public class DirtPile : MonoBehaviour
 {
 
     public Crop myCrop; //the crop that the pile will hold
-
+    public Transform spawnlocation;
     private float deathTimer = 60f;
+    private float cropTimer = 60f;
     public bool hasCrop;
     private void Update() {
         if (!hasCrop) {
             deathTimer -= Time.deltaTime;
             if(deathTimer <= 0) {
+                Destroy(this.gameObject);
+            }
+        } else {
+            cropTimer -= Time.deltaTime;
+            if(cropTimer <= 0) {
+                for(int i = 0; i < GameManager.instance.gameObject.GetComponent<ItemDatabase>().crops.Count; i++) {
+                    if(myCrop == GameManager.instance.gameObject.GetComponent<ItemDatabase>().crops[i].GetComponent<Seed>().seedCrop) {
+                        Instantiate(GameManager.instance.gameObject.GetComponent<ItemDatabase>().crops[i], spawnlocation.position, Quaternion.identity);
+                        break;
+                    }
+                }
                 Destroy(this.gameObject);
             }
         }
